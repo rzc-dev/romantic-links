dab0cf12c0df09e8c12c7e0b0cab039025c35fde
robertoxd159 <robertoxd159>
2026-04-14 00:08:14 -0500

    feat: agregar soporte para música de YouTube

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@theme/tailwindcss' // O el plugin que uses
+import tailwindcss from '@tailwindcss/vite' // <-- ESTE ES EL NOMBRE CORRECTO
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // Activa el plugin de Tailwind v4
+  ],
 })
